299cb9c55118030b847c0f977f1c5cb75605ccf5
Vasily Gusev <vasily.guseff@gmail.com>
2020-03-06 10:40:56 +0300

    feat: add filter if active

--- src/actions/sort.js
+++ src/actions/sort.js
@@ -1,9 +1,16 @@
-import { SORT_BY_NAME, SORT_BY_ACTIVE } from '../constants'
+import { SORT_BY_NAME, SORT_BY_ACTIVE, FILTER_IF_ACTIVE } from '../constants'
 
-export const sortByActive = () => {
+export const sortByActive = val => {
   return {
     type: SORT_BY_ACTIVE,
-    payload: {},
+    payload: val,
+  }
+}
+
+export const filterIfActive = val => {
+  return {
+    type: FILTER_IF_ACTIVE,
+    payload: val,
   }
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { filterIfActive } from '../../actions/sort'
 
 const Header = () => {
-  return <div className="container app-header">This is Header</div>
+  const dispatch = useDispatch()
+
+  const activeOnly = useSelector(state => state.filter.filterIfActive)
+
+  const activeCheckHandle = () => {
+    dispatch(filterIfActive(!activeOnly))
+  }
+
+  return (
+    <div className="container my-3">
+      <h3>RS School Test Table With Black Jack</h3>
+      <form action="\">
+        <div className="row">
+          <div className="col-6">
+            <span className="form-text text-muted">Search</span>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="col-6">
+            <span className="form-text text-muted">Search</span>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail2"
+              aria-describedby="emailHelp"
+            />
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="offset-8 col-4 input-wrapper">
+            <input
+              type="checkbox"
+              defaultChecked={activeOnly}
+              className="mr-3"
+              onChange={activeCheckHandle}
+            />
+            <span className="text-muted">Active members only</span>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="offset-8 col-4 input-wrapper">
+            <input type="checkbox" className="mr-3" />
+            <span className="text-muted">Use virtualization</span>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Header
